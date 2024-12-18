4e8f50c89a8ccaeb55ff963e1ee0ba6195b1bb8f
JustDondo <162633910+CodeDondo@users.noreply.github.com>
2024-12-18 14:41:10 +0100

    changes

--- scripts/riveActions.js
+++ scripts/riveActions.js
@@ -2,9 +2,11 @@ import { Dance } from "./riveController.js";
 
 let danceButton = document.getElementById('danceButton');
 
+
 const dance = () => {
     Dance.fire();
 }
+
 
 danceButton.addEventListener('click', () => {
     dance();    
